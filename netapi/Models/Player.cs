96d615ec1084a2c1bc347577ec94961f3b9fd144
ColtonJones197 <ckjones97@ksu.edu>
2023-10-31 16:11:10 -0500

    translates players

--- netapi/Models/Player.cs
+++ netapi/Models/Player.cs
@@ -8,5 +8,15 @@
         public string? Title { get; set; }
         public string? Status { get; set; }
         public string? Name { get; set; }
+
+        public Player(string username, int chesscomId, string? avatar, string? title, string? status, string? name)
+        {
+            Username = username;
+            ChesscomId = chesscomId;
+            Avatar = avatar;
+            Title = title;
+            Status = status;
+            Name = name;
+        }
     }
 }
--- netapi/SqlPlayerRepository.cs
+++ netapi/SqlPlayerRepository.cs
@@ -1,4 +1,6 @@
-﻿using netapi.Models;
+﻿using Microsoft.Data.SqlClient;
+using netapi.Models;
+using System.Data;
 
 namespace netapi
 {
@@ -29,7 +31,45 @@ namespace netapi
 
         public IReadOnlyList<Player> RetrievePlayers()
         {
-            throw new NotImplementedException();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                using (var command = new SqlCommand("Chesscom.CreatePlayer", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    connection.Open();
+
+                    using (var reader = command.ExecuteReader())
+                        return TranslatePlayers(reader);
+                }
+            }
+        }
+
+        private IReadOnlyList<Player> TranslatePlayers(SqlDataReader reader)
+        {
+            var players = new List<Player>();
+
+            //var playerIdOrd = reader.GetOrdinal("PlayerId");
+            var usernameOrd = reader.GetOrdinal("Username");
+            var chesscomIdOrd = reader.GetOrdinal("ChesscomId");
+            var avatarOrd = reader.GetOrdinal("Avatar");
+            var titleOrd = reader.GetOrdinal("Title");
+            var statusOrd = reader.GetOrdinal("Status");
+            var nameOrd = reader.GetOrdinal("Name");
+
+            while(reader.Read())
+            {
+                players.Add(new Player(
+                    reader.GetString(usernameOrd),
+                    reader.GetInt32(chesscomIdOrd),
+                    reader.GetString(avatarOrd),
+                    reader.GetString(titleOrd),
+                    reader.GetString(statusOrd),
+                    reader.GetString(nameOrd)
+                    ));
+            }
+
+            return players;
         }
     }
 }
