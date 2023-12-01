992744b9356de9265a890cd1e63fc676c8d3cbb7
ColtonJones197 <ckjones97@ksu.edu>
2023-12-01 17:27:33 -0600

    bug fix

--- db/functions/Chesscom.AllGamesForPlayerByColor.sql
+++ db/functions/Chesscom.AllGamesForPlayerByColor.sql
@@ -1,8 +1,8 @@
-CREATE FUNCTION Chesscom.AllGamesForPlayerByColor(@Username NVARCHAR(128))
+CREATE OR ALTER FUNCTION Chesscom.AllGamesForPlayerByColor(@Username NVARCHAR(128))
 RETURNS TABLE
 AS
 RETURN(
-SELECT IIF(PW.Username = @Username, N'W', N'B') AS UserColor, G.*
+SELECT IIF(PW.Username = @Username, N'W', N'B') AS UserColor, PW.Username AS WhitePlayer, PB.Username AS BlackPlayer, G.*
 FROM Chesscom.Game G
 	INNER JOIN Chesscom.Player PW ON PW.PlayerId = G.WhitePlayerId
 	INNER JOIN Chesscom.Player PB ON PB.PlayerId = G.BlackPlayerId
