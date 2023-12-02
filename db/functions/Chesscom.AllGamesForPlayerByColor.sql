39ca5a0fca66cd739050a9fe6fdd7f7656ce288f
ColtonJones197 <ckjones97@ksu.edu>
2023-12-01 20:55:21 -0600

    gitignore

--- db/functions/Chesscom.AllGamesForPlayerByColor.sql
+++ db/functions/Chesscom.AllGamesForPlayerByColor.sql
@@ -7,5 +7,6 @@ FROM Chesscom.Game G
 	INNER JOIN Chesscom.Player PW ON PW.PlayerId = G.WhitePlayerId
 	INNER JOIN Chesscom.Player PB ON PB.PlayerId = G.BlackPlayerId
 WHERE PW.Username = @Username OR PB.Username = @Username
+	AND G.IsRemoved = 0
 );
 GO
